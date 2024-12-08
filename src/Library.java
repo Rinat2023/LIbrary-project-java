4b222193e633cbdb8a41be2215fa4849164727db
rinat2023 <riko14040000@gmail.com>
2024-12-08 12:44:59 +0600

    to new repo

--- src/Library.java
+++ src/Library.java
@@ -1,15 +1,16 @@
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.Arrays;
 
 public class Library {
     private String name;
     private String address;
-    private String balance;
+    private int balance;
     private Book[] books;
     private Bron[] brons;
     private Client[] clients;
 
-    public Library(String name, String address, String balance, Book[] books, Bron[] brons, Client[] clients) {
+    public Library(String name, String address, int balance, Book[] books, Bron[] brons, Client[] clients) {
         this.name = name;
         this.address = address;
         this.balance = balance;
@@ -21,10 +22,29 @@ public class Library {
     public Library() {
     }
 
-    // посмотреть доступные книги - Нурис
-    // посмотреть занятые книги - Марсель
-    // Забронировать книгу - Ринат
+    // _____________посмотреть доступные книги - Нурис_______________
+    public void showAviableBooks() {
+        int count = 0;
+        for (Book book : books) {
+            if (book.isAvailable() == false) {
+                System.out.println(count + ". " + book.getTitle());
+                count++;
+            }
+        }
+    }
 
+    // _____________посмотреть занятые книги - Марсель______________-
+    public void showBookedBooks() {
+        int count = 0;
+        for (Book book : books) {
+            if (book.isAvailable() == true) {
+                System.out.println(count + ". " + book.getTitle());
+                count++;
+            }
+        }
+    }
+
+    // __________Забронировать книгу - Ринат_________________
     public String bronBook(int bookId, int clientId, LocalDate startDate, LocalDate endDate) {
         Client client = null;
         for (int i = 0; i < clients.length; i++) {
@@ -36,21 +56,50 @@ public class Library {
         Book book = null;
         for (int i = 0; i < books.length; i++) {
             if (bookId == i) {
-                book = books[i];
                 if (!books[i].isAvailable()) {
                     books[i].setAvailable(true);
                 }
+                book = books[i];
             }
-            books[i] = book;
         }
         Bron bronBook = new Bron(client, book, startDate, endDate, 0);
-        Bron[] newBrons = new Bron[brons.length + 1];
-        newBrons[brons.length - 1] = bronBook;
+        Bron[] newBrons = Arrays.copyOf(brons, brons.length + 1);
+        newBrons[newBrons.length - 1] = bronBook;
         brons = newBrons;
         return "Книга успешно забронировано";
     }
 
-    // Вернуть книгу - Алтынай
+    // ____________Вернуть книгу - Алтынай___________
+    public void returnBook(int bookId) {
+        Bron findedBron = null;
+        Bron[] newBrons = new Bron[brons.length - 1];
+        for (int i = 0; i < brons.length; i++) {
+            if (i == bookId) {
+                findedBron = brons[i];
+                continue;
+            } else {
+                newBrons[i] = brons[i];
+            }
+        }
+        for (Book returnedBook : books){
+            if (findedBron.getBook().getTitle().equals(returnedBook.getTitle()) ){
+                returnedBook.setAvailable(false);
+            }
+        }
+        if (findedBron.getEndDate().isAfter(LocalDate.now())){
+            Period period = Period.between(findedBron.getEndDate(), LocalDate.now());
+            for (Client client : clients){
+                if (client.getFullName().equals(findedBron.getClient().getFullName())){
+                   int clientBalance = client.getBalance();
+                   client.setBalance(clientBalance - (period.getDays()) * 100);
+                }
+            }
+        }
+        brons = newBrons;
+        System.out.println("Возврат книги прошла успешно");
+    }
+
+
     // Выход - я
 
     public String getName() {
@@ -69,11 +118,11 @@ public class Library {
         this.address = address;
     }
 
-    public String getBalance() {
+    public int getBalance() {
         return balance;
     }
 
-    public void setBalance(String balance) {
+    public void setBalance(int balance) {
         this.balance = balance;
     }
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,94 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //objects - Beka
-        //menu - Elzar
+
+        //_______________books_____________________
+        Book Math = new Book("Math", "Rinat", 300, "Learning", LocalDate.of(1999, 12, 3), false);
+        Book History = new Book("History", "Rinat", 566, "Learning", LocalDate.of(1999, 12, 3), false);
+        Book Chemistry = new Book("Chemistry", "Rinat", 499, "Learning", LocalDate.of(1999, 12, 3), false);
+        Book Geography = new Book("Geography", "Rinat", 111, "Learning", LocalDate.of(1999, 12, 3), false);
+        Book Phisics = new Book("Phisics", "Rinat", 400, "Learning", LocalDate.of(1999, 12, 3), false);
+        Book Literature = new Book("Literature", "Rinat", 1000, "Learning", LocalDate.of(1999, 12, 3), false);
+
+        Book[] books = {Math, History, Chemistry, Geography, Phisics, Literature};
+
+        //_______________clients__________________
+        Client Rinat = new Client("Rinat", 20, 1000);
+        Client Elzar = new Client("Elzar", 20, 1000);
+        Client Marsel = new Client("Marsel", 20, 1000);
+
+        Client[] clients = {Rinat, Elzar, Marsel};
+
+        //_____________Library_____________
+        Library MetaLibrary = new Library("MetaLibrary", "Gorkogo_97", 1000000, books, new Bron[0], clients);
+
+        //________________MENU_____________________
+        Scanner scanner = new Scanner(System.in);
+        int choose = 0;
+        while (choose != 5) {
+            getMenu();
+            choose = scanner.nextInt();
+            switch (choose) {
+                case 1: {
+                    MetaLibrary.showAviableBooks();
+                    break;
+                }
+                case 2: {
+                    MetaLibrary.showBookedBooks();
+                    break;
+                }
+                case 3: {
+                    System.out.println("Каким клиентом вы являетесь?");
+                    getClient(clients);
+                    int numOfClient = scanner.nextInt();
+                    System.out.println("Выберите книгу");
+                    getBooks(books);
+                    int numOfBook = scanner.nextInt();
+                    System.out.println("До какого числа будете бронировать книгу?");
+                    System.out.println("Год: ");
+                    int year = scanner.nextInt();
+                    System.out.println("Месяц: ");
+                    int month = scanner.nextInt();
+                    System.out.println("День: ");
+                    int day = scanner.nextInt();
+                    System.out.println(MetaLibrary.bronBook(numOfBook, numOfClient, LocalDate.now(), LocalDate.of(year, month, day)));
+                    break;
+                }
+                case 4:{
+                    System.out.println("Какую книгу вы бронировали?");
+                    MetaLibrary.showBookedBooks();
+                    int bookNum = scanner.nextInt();
+                    MetaLibrary.returnBook(bookNum);
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void getMenu() {
+        System.out.println("Выберите действие:");
+        System.out.println("(1) Посмотреть список всех доступных книг");
+        System.out.println("(2) Посмотреть список всех занятых книг");
+        System.out.println("(3) Забронировать книгу");
+        System.out.println("(4) Вернуть книгу");
+        System.out.println("(5) Выйти");
+    }
+
+    public static void getBooks(Book[] books) {
+        int count = 0;
+        for (Book book : books) {
+            System.out.println(count + ". " + book.getTitle());
+            count++;
+        }
+    }
+
+    public static void getClient(Client[] clients) {
+        int count = 0;
+        for (Client client : clients) {
+            System.out.println(count + ". " + client.getFullName());
+            count++;
+        }
     }
 }
